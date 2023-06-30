0bec8de22c817ef30f3d49a8a42c2cdda44434ac
ChrisRDavila <crossdavila@gmail.com>
2023-06-30 16:16:38 -0700

    Add background image

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "./Header";
-import CoffeeControl from "./CoffeeControl"
+// import CoffeeControl from "./CoffeeControl"
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <CoffeeControl />
-      
     </React.Fragment>
 
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App';
+import CoffeeControl  from './components/CoffeeControl';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <CoffeeControl/>
   </React.StrictMode>
 );
 
