0b19253a4b23f58318945a24b886fd1ed61a1c1c
ChrisRDavila <crossdavila@gmail.com>
2023-06-30 12:06:40 -0700

    set up coffeeDetails with onCoffee buy, delete and edit func props as well as logic for out of stock once amount <= 0

---
+++ src/components/CoffeeDetails.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function CoffeeDetails(props) {
+  const { coffee, onClickingDelete, onClickingBuy } = props;
+
+  return (
+    <React.Fragment>
+    <h1>Details for: {coffee.name}</h1>
+    <h3>From: {coffee.origin}</h3>
+    <h3>Price per Pound: {coffee.price}</h3>
+    <h3>Roast: {coffee.roast}</h3>
+    <h3>In Stock: {coffee.amount}</h3>
+    <hr/>
+    
+    <button onClick={ props.onClickingEdit }>Edit Inventory</button>
+    <button onClick={() => onClickingDelete(coffee.id)}>Delete from Inventory</button>
+
+    {
+    coffee.amount > 0 ? (
+    <button onClick={() => onClickingBuy(coffee.id)}>Buy 1 pound</button>) : (
+    <h1>{coffee.name} is out of stock</h1>)
+    }
+
+    </React.Fragment>
+  );
+}
+
+CoffeeDetails.propTypes = {
+  coffee: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func,
+  onClickingBuy: PropTypes.func
+}
+
+export default CoffeeDetails;
