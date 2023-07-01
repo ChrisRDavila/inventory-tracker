b2b385591dd1e9f71798ff8bc1401996b0cf08bc
ChrisRDavila <crossdavila@gmail.com>
2023-07-01 00:06:49 -0700

    add image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
+import coffeeIMG from "./../img/coffeeIMG.jpeg"
 
 function Header(){
   return (
     <React.Fragment>
+      <img src={coffeeIMG} alt="An image of coffee" />
       <h1>Jake's Coffee inventory tracker</h1>
     </React.Fragment>
   );
